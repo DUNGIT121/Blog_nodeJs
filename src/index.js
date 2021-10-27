8b442f45c35d239ae79ba48941fd37ed303596d1
DUNGIT121 <levandung9899@gmail.com>
2021-10-27 09:55:46 +0700

    fixx husky 3

--- src/index.js
+++ src/index.js
@@ -26,8 +26,7 @@ app.engine(
     }),
 );
 
-        app.set('view engine',
-        'hbs');
+app.set('view engine','hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // routes init
